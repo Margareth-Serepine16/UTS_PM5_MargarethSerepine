1aae5cd1d7cc8c29c254098e0be77cda75146cfb
DP5-Margareth-022 <margareths4167@gmail.com>
2024-10-28 22:43:13 +0700

    th commit

--- app/src/main/java/com/example/utslab5mobile_margareth/MainActivity.kt
+++ app/src/main/java/com/example/utslab5mobile_margareth/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.utslab5mobile_margareth
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import android.widget.RadioGroup
 import android.widget.RadioButton
@@ -13,6 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val tv_finishbtn = findViewById<Button>(R.id.finishbtn)
+        tv_finishbtn.setOnClickListener {
+            Intent(this, Result::class.java).also {
+                startActivity(it)
+            }
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/utslab5mobile_margareth/StartPage.kt
+++ app/src/main/java/com/example/utslab5mobile_margareth/StartPage.kt
@@ -1,6 +1,8 @@
 package com.example.utslab5mobile_margareth
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class StartPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_start_page)
+
+        val tv_startbtn = findViewById<Button>(R.id.tv_startbtn)
+        tv_startbtn.setOnClickListener {
+            Intent(this, MainActivity::class.java).also {
+                startActivity(it)
+            }
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
